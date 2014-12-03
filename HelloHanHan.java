8cbd7d2cea93c04e5bb6b1ce1c93cfa7c0c154bd
hansqrd <hwang@mba2015.hbs.edu>
2014-12-02 19:44:56 -0500

    Update HelloHanHan.java

--- HelloHanHan.java
+++ HelloHanHan.java
@@ -1,6 +1,7 @@
 public class HelloHanHan {
   public static void main(String[] args)
   {
-    System.out.println("Hello HanHan!");
+    System.out.println("Hello HanHan Wang!");
+    System.out.println("Hello Owen!");
   }
 }
